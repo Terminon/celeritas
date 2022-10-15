f2cb7c31953b17344784ce7e7c32a78bb9d357bd
Torsten <magus@zauberturm.de>
2022-10-15 11:54:09 +0200

    Fix download functionality

--- response-utils.go
+++ response-utils.go
@@ -1,78 +1,78 @@
 package celeritas
 
 import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"net/http"
-	"path"
-	"path/filepath"
+    "encoding/json"
+    "encoding/xml"
+    "fmt"
+    "net/http"
+    "path"
+    "path/filepath"
 )
 
 func (c *Celeritas) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
-	out, err := json.MarshalIndent(data, "", "\t")
-	if err != nil {
-		return err
-	}
+    out, err := json.MarshalIndent(data, "", "\t")
+    if err != nil {
+        return err
+    }
 
-	if len(headers) > 0 {
-		for key, value := range headers[0] {
-			w.Header()[key] = value
-		}
-	}
+    if len(headers) > 0 {
+        for key, value := range headers[0] {
+            w.Header()[key] = value
+        }
+    }
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	_, err = w.Write(out)
-	if err != nil {
-		return err
-	}
-	return nil
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(status)
+    _, err = w.Write(out)
+    if err != nil {
+        return err
+    }
+    return nil
 }
 
 func (c *Celeritas) WriteXML(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
-	out, err := xml.MarshalIndent(data, "", "    ")
-	if err != nil {
-		return err
-	}
+    out, err := xml.MarshalIndent(data, "", "    ")
+    if err != nil {
+        return err
+    }
 
-	if len(headers) > 0 {
-		for key, value := range headers[0] {
-			w.Header()[key] = value
-		}
-	}
+    if len(headers) > 0 {
+        for key, value := range headers[0] {
+            w.Header()[key] = value
+        }
+    }
 
-	w.Header().Set("Content-Type", "application/xml")
-	w.WriteHeader(status)
-	_, err = w.Write(out)
-	if err != nil {
-		return err
-	}
-	return nil
+    w.Header().Set("Content-Type", "application/xml")
+    w.WriteHeader(status)
+    _, err = w.Write(out)
+    if err != nil {
+        return err
+    }
+    return nil
 }
 
 func (c *Celeritas) DownloadFile(w http.ResponseWriter, r *http.Request, pathToFile, fileName string) error {
-	fullPath := path.Join(pathToFile, fileName)
-	fileToServe := filepath.Clean(fullPath)
+    fullPath := path.Join(pathToFile, fileName)
+    fileToServe := filepath.Clean(fullPath)
 
-	w.Header().Set("Content-Type", fmt.Sprintf("attachment; file=\"%s\"", fileName))
-	http.ServeFile(w, r, fileToServe)
+    w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; file=\"%s\"", fileName))
+    http.ServeFile(w, r, fileToServe)
 
-	return nil
+    return nil
 }
 
 func (c *Celeritas) ErrorStatus(w http.ResponseWriter, status int) {
-	http.Error(w, http.StatusText(status), status)
+    http.Error(w, http.StatusText(status), status)
 }
 func (c *Celeritas) Error404(w http.ResponseWriter, r *http.Request) {
-	c.ErrorStatus(w, http.StatusNotFound)
+    c.ErrorStatus(w, http.StatusNotFound)
 }
 func (c *Celeritas) Error500(w http.ResponseWriter, r *http.Request) {
-	c.ErrorStatus(w, http.StatusInternalServerError)
+    c.ErrorStatus(w, http.StatusInternalServerError)
 }
 func (c *Celeritas) ErrorUnauthorized(w http.ResponseWriter, r *http.Request) {
-	c.ErrorStatus(w, http.StatusUnauthorized)
+    c.ErrorStatus(w, http.StatusUnauthorized)
 }
 func (c *Celeritas) ErrorForbidden(w http.ResponseWriter, r *http.Request) {
-	c.ErrorStatus(w, http.StatusForbidden)
+    c.ErrorStatus(w, http.StatusForbidden)
 }
