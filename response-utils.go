b1b5ff3c48e3d6715188a08d452fb6573dce1fcd
Torsten <magus@zauberturm.de>
2022-10-15 11:27:06 +0200

    78.JSON,XML and other response types e.g. Download

--- response-utils.go
+++ response-utils.go
@@ -2,7 +2,11 @@ package celeritas
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"fmt"
 	"net/http"
+	"path"
+	"path/filepath"
 )
 
 func (c *Celeritas) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
@@ -24,4 +28,51 @@ func (c *Celeritas) WriteJSON(w http.ResponseWriter, status int, data interface{
 		return err
 	}
 	return nil
+}
+
+func (c *Celeritas) WriteXML(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := xml.MarshalIndent(data, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) DownloadFile(w http.ResponseWriter, r *http.Request, pathToFile, fileName string) error {
+	fullPath := path.Join(pathToFile, fileName)
+	fileToServe := filepath.Clean(fullPath)
+
+	w.Header().Set("Content-Type", fmt.Sprintf("attachment; file=\"%s\"", fileName))
+	http.ServeFile(w, r, fileToServe)
+
+	return nil
+}
+
+func (c *Celeritas) ErrorStatus(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+func (c *Celeritas) Error404(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusNotFound)
+}
+func (c *Celeritas) Error500(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusInternalServerError)
+}
+func (c *Celeritas) ErrorUnauthorized(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusUnauthorized)
+}
+func (c *Celeritas) ErrorForbidden(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusForbidden)
 }
