901488dcfdd8d89b5a65faef5c75a93b29391a08
Torsten <magus@zauberturm.de>
2022-12-07 11:13:46 +0100

    Lecture 118: Starting to implement celeritas new functionality

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"errors"
-	"os"
+    "errors"
+    "os"
 
-	"github.com/Terminon/celeritas"
-	"github.com/fatih/color"
+    "github.com/Terminon/celeritas"
+    "github.com/fatih/color"
 )
 
 const version = "1.0.0"
@@ -13,84 +13,90 @@ const version = "1.0.0"
 var cel celeritas.Celeritas
 
 func main() {
-	var message string
-	arg1, arg2, arg3, err := validateInput()
-	if err != nil {
-		exitGracefully(err)
-	}
+    var message string
+    arg1, arg2, arg3, err := validateInput()
+    if err != nil {
+        exitGracefully(err)
+    }
 
-	setup()
+    setup()
 
-	switch arg1 {
-	case "help":
-		showHelp()
+    switch arg1 {
+    case "help":
+        showHelp()
 
-	case "version":
-		color.Yellow("Application version: " + version)
+    case "new":
+        if arg2 == "" {
+            exitGracefully(errors.New("new requires an application name"))
+        }
+        doNew(arg2)
 
-	case "migrate":
-		if arg2 == "" {
-			arg2 = "up"
-		}
-		err = doMigrate(arg2, arg3)
-		if err != nil {
-			exitGracefully(err)
-		}
-		message = "Migrations complete!"
+    case "version":
+        color.Yellow("Application version: " + version)
 
-	case "make":
-		if arg2 == "" {
-			exitGracefully(errors.New("make requires a subcommand: (migration|model|handler)"))
-		}
-		err = doMake(arg2, arg3)
-		if err != nil {
-			exitGracefully(err)
-		}
+    case "migrate":
+        if arg2 == "" {
+            arg2 = "up"
+        }
+        err = doMigrate(arg2, arg3)
+        if err != nil {
+            exitGracefully(err)
+        }
+        message = "Migrations complete!"
 
-	default:
-		showHelp()
-	}
+    case "make":
+        if arg2 == "" {
+            exitGracefully(errors.New("make requires a subcommand: (migration|model|handler)"))
+        }
+        err = doMake(arg2, arg3)
+        if err != nil {
+            exitGracefully(err)
+        }
 
-	exitGracefully(nil, message)
+    default:
+        showHelp()
+    }
+
+    exitGracefully(nil, message)
 }
 
 func validateInput() (string, string, string, error) {
-	var arg1, arg2, arg3 string
+    var arg1, arg2, arg3 string
 
-	if len(os.Args) > 1 {
-		arg1 = os.Args[1]
+    if len(os.Args) > 1 {
+        arg1 = os.Args[1]
 
-		if len(os.Args) >= 3 {
-			arg2 = os.Args[2]
-		}
+        if len(os.Args) >= 3 {
+            arg2 = os.Args[2]
+        }
 
-		if len(os.Args) >= 4 {
-			arg3 = os.Args[3]
-		}
-	} else {
-		color.Red("Error: command required")
-		showHelp()
-		return "", "", "", errors.New("command required")
-	}
+        if len(os.Args) >= 4 {
+            arg3 = os.Args[3]
+        }
+    } else {
+        color.Red("Error: command required")
+        showHelp()
+        return "", "", "", errors.New("command required")
+    }
 
-	return arg1, arg2, arg3, nil
+    return arg1, arg2, arg3, nil
 }
 
 func exitGracefully(err error, msg ...string) {
-	message := ""
-	if len(msg) > 0 {
-		message = msg[0]
-	}
+    message := ""
+    if len(msg) > 0 {
+        message = msg[0]
+    }
 
-	if err != nil {
-		color.Red("Error: %v\n", err)
-	}
+    if err != nil {
+        color.Red("Error: %v\n", err)
+    }
 
-	if len(message) > 0 {
-		color.Yellow(message)
-	} else {
-		color.Green("Finished!")
-	}
+    if len(message) > 0 {
+        color.Yellow(message)
+    } else {
+        color.Green("Finished!")
+    }
 
-	os.Exit(0)
+    os.Exit(0)
 }
--- go.mod
+++ go.mod
@@ -9,9 +9,11 @@ require (
 	github.com/alexedwards/scs/v2 v2.4.0
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/bwmarrin/go-alone v0.0.0-20190806015146-742bb55d1631
+	github.com/docker/docker v17.12.0-ce-rc1.0.20200618181300-9dc6525e6118+incompatible
 	github.com/fatih/color v1.13.0
 	github.com/gertd/go-pluralize v0.2.1
 	github.com/go-chi/chi/v5 v5.0.4
+	github.com/go-git/go-git/v5 v5.5.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang-migrate/migrate/v4 v4.14.1
 	github.com/gomodule/redigo v1.8.9
@@ -26,25 +28,47 @@ require (
 
 require (
 	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/Microsoft/hcsshim v0.9.5 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20221026131551-cf6655e29de4 // indirect
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
 	github.com/andybalholm/cascadia v1.1.0 // indirect
+	github.com/cloudflare/circl v1.1.0 // indirect
+	github.com/containerd/continuity v0.3.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
 	github.com/go-test/deep v1.0.8 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.0 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.1.1 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.8.1 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/pjbgf/sha1cd v0.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/skeema/knownhosts v1.1.0 // indirect
 	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
 	github.com/vanng822/css v1.0.1 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
-	golang.org/x/text v0.3.6 // indirect
+	github.com/xanzy/ssh-agent v0.3.2 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
