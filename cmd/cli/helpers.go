22771fc3dcf2b34a9d0745a1b83af92bbd8bf19e
Terminon <magus@zauberturm.de>
2022-12-21 11:41:07 +0100

    continue working on new project creation feature

--- cmd/cli/helpers.go
+++ cmd/cli/helpers.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/joho/godotenv"
@@ -71,4 +74,47 @@ func showHelp() {
 	make session		  - creates a table in the database as a session store
 	make mail <name>      - create 2 starter mail templates in the mail directory
 	`)
+}
+
+func updateSourceFiles(path string, fi os.FileInfo, err error) error {
+	// check for an error before doing anything else
+	if err != nil {
+		return err
+	}
+	// check if current file is directory
+	if fi.IsDir() {
+		return nil
+	}
+
+	// only check go files
+	matched, err := filepath.Match("*.go", fi.Name())
+	if err != nil {
+		return err
+	}
+
+	// we have a matching file
+	if matched {
+		// read file contents
+		read, err := os.ReadFile(path)
+		if err != nil {
+			exitGracefully(err)
+		}
+		newContents := strings.Replace(string(read), "myapp", appURL, -1)
+
+		// write changed file
+		err = ioutil.WriteFile(path, []byte(newContents), 0)
+		if err != nil {
+			exitGracefully(err)
+		}
+	}
+
+	return nil
+}
+
+func updateSource() {
+	// walk entire project folder, including subfolders
+	err := filepath.Walk(".", updateSourceFiles)
+	if err != nil {
+		exitGracefully(err)
+	}
 }
--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -1,61 +1,88 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strings"
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
+    "strings"
 
-	"github.com/fatih/color"
-	"github.com/go-git/go-git/v5"
+    "github.com/fatih/color"
+    "github.com/go-git/go-git/v5"
 )
 
+var appURL string
+
 func doNew(appName string) {
-	appName = strings.ToLower(appName)
+    appName = strings.ToLower(appName)
+    appURL = appName
 
-	// sanitize the application name ( convert url to single word )
-	if strings.Contains(appName, "/") {
-		exploded := strings.SplitAfter(appName, "/")
-		appName = exploded[len(exploded)-1]
-	}
+    // sanitize the application name ( convert url to single word )
+    if strings.Contains(appName, "/") {
+        exploded := strings.SplitAfter(appName, "/")
+        appName = exploded[len(exploded)-1]
+    }
 
-	log.Println("App name is", appName)
+    log.Println("App name is", appName)
 
-	// git clone the skeleton application
-	color.Green("\tCloning repository...")
-	_, err := git.PlainClone("./"+appName, false, &git.CloneOptions{
-		URL:      "https://Terminon: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@github.com/Terminon/celeritas-app",
-		Progress: os.Stdout,
-		Depth:    1,
-	})
-	if err != nil {
-		exitGracefully(err)
-	}
+    // git clone the skeleton application
+    color.Green("\tCloning repository...")
+    _, err := git.PlainClone("./"+appName, false, &git.CloneOptions{
+        URL:      "https://Terminon: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@github.com/Terminon/celeritas-app",
+        Progress: os.Stdout,
+        Depth:    1,
+    })
+    if err != nil {
+        exitGracefully(err)
+    }
 
-	// remove the .git directory
-	err = os.RemoveAll(fmt.Sprintf("./%s/.git", appName))
-	if err != nil {
-		exitGracefully(err)
-	}
-	// create a ready to go .env file
-	color.Yellow("\tCreating .env file...")
-	data, err := templateFS.ReadFile("templates/env.txt")
-	if err != nil {
-		exitGracefully(err)
-	}
-	env := string(data)
-	env = strings.ReplaceAll(env, "${APP_NAME}", appName)
-	env = strings.ReplaceAll(env, "${KEY}", cel.RandomString(32))
+    // remove the .git directory
+    err = os.RemoveAll(fmt.Sprintf("./%s/.git", appName))
+    if err != nil {
+        exitGracefully(err)
+    }
+    // create a ready to go .env file
+    color.Yellow("\tCreating .env file...")
+    data, err := templateFS.ReadFile("templates/env.txt")
+    if err != nil {
+        exitGracefully(err)
+    }
+    env := string(data)
+    env = strings.ReplaceAll(env, "${APP_NAME}", appName)
+    env = strings.ReplaceAll(env, "${KEY}", cel.RandomString(32))
 
-	err = copyDataToFile([]byte(env), fmt.Sprintf("./%s/.env", appName))
-	if err != nil {
-		exitGracefully(err)
-	}
+    err = copyDataToFile([]byte(env), fmt.Sprintf("./%s/.env", appName))
+    if err != nil {
+        exitGracefully(err)
+    }
 
-	// update the go.mod file
+    // update the go.mod file
+    color.Yellow("\tCreating go.mod file...")
+    _ = os.Remove("./" + appName + "/go.mod")
 
-	// update existing .go files with correct /name/imports
+    data, err = templateFS.ReadFile("templates/go.mod.txt")
+    if err != nil {
+        exitGracefully(err)
+    }
+    mod := string(data)
+    mod = strings.ReplaceAll(mod, "${APP_NAME}", appURL)
+    err = copyDataToFile([]byte(mod), "./"+appName+"/go.mod")
+    if err != nil {
+        exitGracefully(err)
+    }
 
-	// run go mod tidy in the project directory
+    // update existing .go files with correct /name/imports
+    color.Yellow("\tUpdating source files...")
+    os.Chdir("./" + appName)
+    updateSource()
+
+    // run go mod tidy in the project directory
+    color.Yellow("\tRunning go mod tidy")
+    cmd := exec.Command("go", "mod", "tidy")
+    err = cmd.Start()
+    if err != nil {
+        exitGracefully(err)
+    }
+    color.Green("Done building " + appURL)
 
 }
